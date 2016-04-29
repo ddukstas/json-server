90513c974f781069af6e967cf6095e4c9657d94b
Dominykas Dukštas <dominykas@zet.lt>
2016-04-29 14:35:58 +0300

    improve router to parse query params

--- src/server/rewriter.js
+++ src/server/rewriter.js
@@ -1,10 +1,22 @@
 var express = require('express')
 
+function getQueryObj(route) {
+  if (route.indexOf('?') !== -1) {
+    var query = route.slice(route.indexOf('?') + 1);
+    var result = {};
+    query.split("&").forEach(function (part) {
+      var item = part.split("=");
+      result[item[0]] = decodeURIComponent(item[1]);
+    });
+    return result;
+  }
+}
+
 module.exports = function (routes) {
   var router = express.Router()
 
-  Object.keys(routes).forEach(function (route) {
 
+  Object.keys(routes).forEach(function (route) {
     if (route.indexOf(':') !== -1) {
       router.all(route, function (req, res, next) {
         // Rewrite target url using params
@@ -12,12 +24,14 @@ module.exports = function (routes) {
         for (var param in req.params) {
           target = target.replace(':' + param, req.params[param])
         }
+        req.query = getQueryObj(routes[route]);
         req.url = target
         next()
       })
     } else {
       router.all(route + '*', function (req, res, next) {
         // Rewrite url by replacing prefix
+        req.query = getQueryObj(routes[route]);
         req.url = req.url.replace(route, routes[route])
         next()
       })
